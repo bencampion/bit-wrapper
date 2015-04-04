c7264c5a7c1e16ff15ff7f2018bdd59cdabaa773
Ben Campion <bencampion@users.noreply.github.com>
2015-04-04 17:33:56 +0100

    Removed unused import from ByteBufferDecoder

--- src/main/java/uk/recurse/bitwrapper/decoder/ByteBufferDecoder.java
+++ src/main/java/uk/recurse/bitwrapper/decoder/ByteBufferDecoder.java
@@ -5,8 +5,6 @@ import uk.recurse.bitwrapper.Wrapper;
 import java.lang.reflect.Method;
 import java.nio.ByteBuffer;
 
-import static com.google.common.base.Preconditions.checkArgument;
-
 public class ByteBufferDecoder implements Decoder<ByteBuffer> {
 
     @Override
